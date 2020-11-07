3d185440c9de2a74454e2eca2ea843ffec321905
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-11-07 19:48:24 -0300

    kwydown variables

--- game.js
+++ game.js
@@ -1,17 +1,27 @@
+// Variables
+
 var canvas = null,
 ctx = null;
 var x = 50;
 var y = 50;
+var lastPress = null;
+
+
+//Canvas Background
 
     function paint(ctx) {
         ctx.fillStyle = '#000';
         ctx.fillRect(0, 0, 300, 150);
+        
         // ctx.fillRect(0, 0, 300, 150);
 
         ctx.fillStyle = '#0f0';
         ctx.fillRect(x, y, 10, 10);
 
     }
+
+    //Movement
+
     function act(){
         x += 0.5;
         if (x > canvas.width){
@@ -30,6 +40,13 @@ var y = 50;
         ctx = canvas.getContext('2d');
         run(ctx);
     }
+
+    document.addEventListener('keydown', function(evt) {
+        lastPress = evt.which;
+    }, false);
+
+
+// Window functions
 
 window.addEventListener('load', init, false);
 window.requestAnimationFrame = (function () { return window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function (callback) { window.setTimeout(callback, 17); }; }());

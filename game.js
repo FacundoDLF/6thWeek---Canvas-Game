e71a6a449e6f8256310534f2641c49452e111ca6
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-11-07 19:15:06 -0300

    functin run

--- game.js
+++ game.js
@@ -6,15 +6,23 @@ var y = 50;
     function paint(ctx) {
         ctx.fillStyle = '#000';
         ctx.fillRect(0, 0, 100, 60);
+        // ctx.fillRect(0, 0, 300, 150);
 
         ctx.fillStyle = '#0f0';
         ctx.fillRect(x, y, 10, 10);
+
     }
+
+    function run() {
+        window.requestAnimationFrame(run);
+        act();
+        paint(ctx);
+        }
 
     function init() {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
-        paint(ctx);
+        run(ctx);
     }
 
 window.addEventListener('load', init, false);

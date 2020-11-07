e90b48f66311b821ea7dd60cf1fd71e4b167d1a4
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-11-07 19:06:20 -0300

    Moving Canvas

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 var canvas = null,
 ctx = null;
+var x = 50;
+var y = 50;
 
     function paint(ctx) {
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, 100, 60);
+
         ctx.fillStyle = '#0f0';
-        ctx.fillRect(50, 50, 100, 60);
+        ctx.fillRect(x, y, 10, 10);
     }
+
     function init() {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
